1fec00c255eb7c40cbbd838c3181b31da8de2af7
kushal <kushalbhetuwal@gmail.com>
2023-06-13 15:24:56 +1000

    1st commit

---
+++ kushal/src/component/navigation/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Navigation =()=>{
+    return(
+        <nav className = "ma0 mt0">
+            <p style ={{display:'flex', justifyContent: 'flex-end'}}className = "f3 pa3 underline black dim pointer">Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;

6fd1ceb342741e948d4319a0b4f71d09b700666c
kushal <kushalbhetuwal@gmail.com>
2023-06-13 21:34:38 +1000

    added particles-bg

--- kushal/src/App.js
+++ kushal/src/App.js
@@ -5,10 +5,13 @@ import 'tachyons';
 import Logo from './component/Logo/logo'
 import ImageLinkForm from './component/ImageLinkForm/imagelinkform';
 import Rank from './component/Rank/rank'
+import ParticlesBg from 'particles-bg';
 class App extends Component {
   render(){
     return (
       <div className="App">
+          
+        <ParticlesBg type="lines" bg={true} />
         
         <Navigation/>
         <Logo/>

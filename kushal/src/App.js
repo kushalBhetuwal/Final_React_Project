9814c18d739fb8aa469e4cc6660a161151d33ff9
kushal <kushalbhetuwal@gmail.com>
2023-06-18 16:05:58 +1000

    2nd changes

--- kushal/src/App.js
+++ kushal/src/App.js
@@ -1,28 +1,41 @@
-import React, { Component}from 'react';
-import './App.css';
-import Navigation from './component/navigation/navigation';
-import 'tachyons';
-import Logo from './component/Logo/logo'
-import ImageLinkForm from './component/ImageLinkForm/imagelinkform';
-import Rank from './component/Rank/rank'
-import ParticlesBg from 'particles-bg';
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./component/navigation/navigation";
+import "tachyons";
+import Logo from "./component/Logo/logo";
+import ImageLinkForm from "./component/ImageLinkForm/imagelinkform";
+import Rank from "./component/Rank/rank";
+import ParticlesBg from "particles-bg";
 
 class App extends Component {
-  render(){
-    
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onSubmit =()=>{
+    console.log("click");
+  }
+
+  onChange =(event)=>{
+    console.log(event);
+    this.setState({input: event.target.value})
+  }
+
+  render() {
     return (
       <div className="App">
         <ParticlesBg type="lines" bg={true} />
-              
-        <Navigation/>
-        <Logo/>
-        <Rank/>
-       <ImageLinkForm/>
-      
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onsubmit ={this.onSubmit} onchange ={this.onChange} />
       </div>
     );
   }
-  
 }
 
 export default App;
